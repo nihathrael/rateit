932ab19257269027bfdf47ce7af262bde8cdbb1a
Thomas Kinnen <kinnen@aviamo.de>
2011-06-02 23:31:15 +0200

    Added basic server and client that work together

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,14 +1,18 @@
 from chatthreadedserver import ChatThreadedServer
 from chatrequesthandler import ChatRequestHandler
 
-if __name__ == '__main__':
-    import sys
-    port = int(sys.argv[1])
-    server = ChatThreadedServer(('localhost', port), ChatRequestHandler)
+import sys
+import pynotify
 
-    try:
-        print "Server starting.  Now waiting."
-        server.serve_forever()
-    except:
-        server.shutdown()
-        print "Shutting down... waiting for all clients to close."
+if __name__ == '__main__':
+	server = ChatThreadedServer(('localhost', 8080), ChatRequestHandler)
+
+	if not pynotify.init("Timekpr notification"):
+		sys.exit(1)
+
+	try:
+		print "Server starting.  Now waiting."
+		server.serve_forever()
+	except:
+		server.shutdown()
+		print "Shutting down... waiting for all clients to close."
